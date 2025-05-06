3684aa6bd4315031b47273f10fb4f60a2cff65bf
b4gabe <gbhilyard@yahoo.com>
2025-05-05 18:46:39 -0700

    Added HTML elements to Navigation and Hero, imported placeholder image for self-portrait

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PlaceHolder from '/src/assets/PlaceHolder.png';
 console.log('Hero working')
 
 function Hero() {
@@ -7,7 +7,25 @@ function Hero() {
 
   return (
     <>
-    
+    <section>
+        <div className='heroPortrait'>
+            <img src={PlaceHolder}/>
+        </div>
+        <div className='heroName'>
+            <h3>Hi, my name is:</h3>
+            <h1>Gabriel Hilyard</h1>
+            <h1>Front-End Engineer</h1>
+        </div>
+        <div className='heroParagraph'>
+            <p>For the past two years, I’ve been dedicated to learning what it 
+                takes to become a front-end engineer. What began as a simple 
+                interest has grown into both a passionate hobby and a promising 
+                career path. While working full-time, I’ve completed the Meta 
+                Front-End Developer course and built several projects along the
+                way. I invite you to explore my portfolio to see my work and the 
+                skills I’ve developed.</p>
+        </div>
+    </section>
     </>
   )
 }
