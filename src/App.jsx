56504d5844b1d750d6679697d2046b8eed2579a4
b4gabe <gbhilyard@yahoo.com>
2025-05-12 18:25:09 -0700

    created seperate component for Project section and imported to app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Nav from './Components/Navigation';
 import Hero from './Components/Hero';
 import Skills from './Components/Skills';
+import Projects from './Components/Projects'
 import Footer from './Components/Footer';
 
 function App() {
---
+++ src/Components/Projects.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+console.log('Projects working')
+
+function Projects() {
+
+
+  return (
+    <>
+      <div className='projectCards'>
+
+
+        <div className='projectCard'>
+          
+        </div>
+        <div className='projectCard'>
+          
+          </div>
+        <div className='projectCard'>
+          
+          </div>
+        <div className='projectCard'>
+          
+          </div>
+        <div className='projectCard'>
+          
+          </div>
+        <div className='projectCard'>
+          
+          </div>
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default Projects;
