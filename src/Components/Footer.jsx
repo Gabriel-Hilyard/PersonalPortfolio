f7de14743a4938f1749f1f70f572c918de4d5e04
b4gabe <gbhilyard@yahoo.com>
2025-05-14 17:31:39 -0700

    Added footer elements

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Snake from './Snake'
+import Snake from './Snake';
+import github from '/src/assets/github.png';
+import linkedin from '/src/assets/linkedin.png';
 console.log('Footer working')
 
 function Footer() {
@@ -8,7 +10,19 @@ function Footer() {
   return (
     <>
     <footer>
-      
+      <div className='socialLinks'>
+      <a href='https://github.com/Gabriel-Hilyard'
+      target='_blank'> <img src={github}
+      alt='github link'/> </a>
+
+      <a href='https://github.com/Gabriel-Hilyard'
+      target='_blank'> <img src={linkedin}
+      alt='linkedin link'/> </a>
+      </div>
+      <div className='email'>
+      <h3><a href="mailto:hilyarddev@yahoo.com">hilyarddev@yahoo.com</a></h3>
+      </div>
+    <a href='https://www.linkedin.com/in/gabriel-hilyard-87a583173'>Created by Gabriel Hilyard</a>
     </footer>
 
     <Snake />
